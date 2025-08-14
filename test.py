37f22ce9a22eff1e2caa3d40c70c49b1e3487290
jskwon-eks-project <jskwon20@icloud.com>
2025-08-14 11:18:13 +0900

    sync VS async

---
+++ test.py
@@ -0,0 +1,27 @@
+def fetch_data():
+    time.sleep(3)  # 3초 동안 CPU 점유하며 멈춤
+    return "data"
+
+def main():
+    print("시작")
+    result = fetch_data()  # 여기서 3초 동안 멈춤 (다른 일 못 함)
+    print(result)
+    print("끝")
+
+print(main())
+
+
+
+# import asyncio
+
+# async def fetch_data():
+#     await asyncio.sleep(3)  # 3초 기다리되 CPU 점유 안 함
+#     return "data"
+
+# async def main():
+#     print("시작")
+#     result = await fetch_data()  # 여기서 대기 중 다른 작업 가능
+#     print(result)
+#     print("끝")
+
+# asyncio.run(main())
